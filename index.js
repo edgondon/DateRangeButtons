a265be58df01966b0722831b75166974c5a30608
Daniel P Landon <dougedd@yahoo.com>
2019-10-01 09:17:22 -0700

    09:17 10.01.2019

--- index.js
+++ index.js
@@ -1,71 +1,39 @@
 'use strict';
 
-const apiKey = 'BQl4kqXRIHiCHp1f1H7kn311pvWMbh11cByBz2nZ';
-const searchURL = 'https://developer.nps.gov/api/v1/parks';
-
-function formatQueryParams(params) {
-    const queryItems = Object.keys(params)
-        .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
-    return queryItems.join('&');
-}
-
-function getParkStatesList(query, maxResults=10) {
-    const params = {
-        stateCode: query,
-        limit: maxResults,
-        api_key: apiKey,
-    };
-    const queryString = formatQueryParams(params)
-    const url = searchURL + '?' + queryString;
-
-    fetch(url)
-        .then(response => {
-            if (response.ok) {
-                return response.json();
-            }
-            throw new Error(response.statusText);
-        })
-        
-        .then(responseJson => consoleResponse(responseJson))
-        .catch(err => {
-            $('#errod').append(`Something went wrong: ${err.message}`);
-        });
-        
-}
-
-
-function consoleResponse(responseJson) {
-
-    console.log("huh?");
-    console.log(responseJson);
-    
-
-    
-
-    for (let i = 0; i < responseJson.data.length; i++) {
-
-       $(".results").append(
-       `<li><p>${responseJson.data[i].fullName}</p>
-       <p>${responseJson.data[i].description}</p>
-        <h3><a href="${responseJson.data[i].url}">${responseJson.data[i].url}</a></h3>
-        </li>`
-        )
+function showEvents(alpha, omega) {
+    $("#datez").append(`<p>${alpha}</p>;
+            <p>${omega}</p>
+            `);
+      
+    }
+  
+    function datesinConsole() {
+        let now = new Date();
+        let month = (now.getMonth() + 1);               
+        let day = now.getDate();
+        if (month < 10) 
+            month = "0" + month;
+        if (day < 10) 
+            day = "0" + day;
+        let today = now.getFullYear() + '-' + month + '-' + day;
+        console.log(today);
     };
 
 
-}
+
 
 
 
 
 function watchEnter() {
+    datesinConsole();
     $('form').submit(event => {
         event.preventDefault();
-        $('#errod').empty();
-        $('.results').empty();
-        const searchTerm = $('#myForm').val();
-        const maxResults = $('#numblum').val();
-        getParkStatesList(searchTerm, maxResults);
+        $('#datez').empty();
+        const alpha = $('#alpha').val();
+        const omega = $('#omega').val();
+        showEvents(alpha, omega);
+
     });
 }
 
